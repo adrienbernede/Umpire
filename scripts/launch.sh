cd14b0a762a3fdb581b6b0cdefb3bc4fa72280c3
Adrien Bernede <bernede1@llnl.gov>
2019-07-15 18:33:52 -0700

    FIX : ...

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -4,5 +4,5 @@ mkdir build_${CLUSTER}_${COMPILER}
 cd build_${CLUSTER}_${COMPILER}
 export CCONF=host-configs/${CLUSTER}/${COMPILER}.cmake 
 cmake -C ../radiuss-ci-config/${CCONF} -C ../${CCONF} ..
-srun -p pdebug -t 5 -N 1 -n 4 cmake --build -j 4 .
+cmake --build -j 4 .
 make test
