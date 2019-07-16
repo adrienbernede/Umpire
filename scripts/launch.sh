328784a0f5e985f28f2ce60ac8dc71ec764c1aa1
Adrien Bernede <bernede1@llnl.gov>
2019-07-16 10:17:11 -0700

    FIX : invisible caracter

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -4,5 +4,5 @@ mkdir build_${CLUSTER}_${COMPILER}
 cd build_${CLUSTER}_${COMPILER}
 export CCONF=host-configs/${CLUSTER}/${COMPILER}.cmake 
 cmake -C ../radiuss-ci-config/${CCONF} -C ../${CCONF} ..
-cmake --build -j 4 .
+cmake --build -j 4 .
 make test
