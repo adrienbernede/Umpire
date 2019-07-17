2fb21aa192cafe3e1b88f1aa6f8782cf5176fe6f
Adrien Bernede <bernede1@llnl.gov>
2019-07-17 09:15:17 -0700

    Added hostname test for future work

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+hostname
+
 mkdir build_${CLUSTER}_${COMPILER}
 cd build_${CLUSTER}_${COMPILER}
+
 export CCONF=host-configs/${CLUSTER}/${COMPILER}.cmake 
+
 cmake -C ../radiuss-ci-config/${CCONF} -C ../${CCONF} ..
 cmake --build -j 4 .
 make test
