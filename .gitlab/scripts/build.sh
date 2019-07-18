fd75e4c85e740b46386fef16ae7d22f61da4036d
Adrien Bernede <bernede1@llnl.gov>
2019-07-18 10:54:47 -0700

    Changing build script, inspired by RAJA

--- .gitlab/scripts/build.sh
+++ .gitlab/scripts/build.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
+##############################################################################
+# Copyright (c) 2018-2019, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory
+#
+# Created by Adrien Bernede, bernede1@llnl.gov
+# LLNL-CODE-747640
+#
+# All rights reserved.
+#
+# This file is part of Umpire.
+#
+# For details, see https://github.com/LLNL/Umpire
+# Please also see the LICENSE file for MIT license.
+##############################################################################
 
 hostname
 
-mkdir build_${CLUSTER}_${COMPILER}
-cd build_${CLUSTER}_${COMPILER}
+BUILD_SUFFIX=${CLUSTER}_${COMPILER}
+CCONF=host-configs/${CLUSTER}/${COMPILER}.cmake 
 
-export CCONF=host-configs/${CLUSTER}/${COMPILER}.cmake 
+rm -rf build_${BUILD_SUFFIX} 2>/dev/null
+mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
 
-cmake -C ../.gitlab/conf/${CCONF} -C ../${CCONF} ..
+cmake \
+  -C ../.gitlab/conf/${CCONF} \
+  -C ../${CCONF} \
+  ..
 cmake --build -j 4 .
-make test
+cmake test
