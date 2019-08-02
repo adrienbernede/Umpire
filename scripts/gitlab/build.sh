a8931f7bb00133eea286351a412deb396df1b88c
Adrien Bernede <bernede1@llnl.gov>
2019-08-02 11:40:13 -0700

    Splitting builds and tests

--- scripts/gitlab/build.sh
+++ scripts/gitlab/build.sh
@@ -14,17 +14,44 @@
 # Please also see the LICENSE file for MIT license.
 ##############################################################################
 
+set -e
+
 hostname
 
-BUILD_SUFFIX="${CLUSTER}_${COMPILER}"
+BUILD_DIRECTORY="build_${CLUSTER}_${COMPILER}"
 CCONF="host-configs/${CLUSTER}/${COMPILER}.cmake" 
 
-rm -rf build_${BUILD_SUFFIX} 2>/dev/null
-mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
+if [[ "${1}" != "--test-only" ]]
+then
+    # If building, then delete everything first
 
-cmake \
-  -C ../.gitlab/conf/${CCONF} \
-  -C ../${CCONF} \
-  ..
-cmake --build . -j 4
-make test
+    rm -rf ${BUILD_DIRECTORY} 2>/dev/null
+    mkdir ${BUILD_DIRECTORY}
+fi
+
+# Assert that build directory exist (mainly for --test-only mode)
+if [[ ! -d ${BUILD_DIRECTORY} ]]
+then
+    echo "Build directory not found : $(pwd)/${BUILD_DIRECTORY}"
+    exit 1
+fi
+
+# Always go to build directory 
+cd ${BUILD_DIRECTORY}
+
+# Build
+if [[ "${1}" != "--test-only" ]]
+then
+    cmake \
+      -C ../.gitlab/conf/${CCONF} \
+      -C ../${CCONF} \
+      ..
+    cmake --build . -j 4
+fi
+
+# Test
+if [[ "${1}" != "--build-only" ]] 
+then
+    # If not building, then don’t
+    make test
+fi
