99449b169173382f6ba1831a6411db554ee53c0e
Adrien M. Bernede <bernede1@llnl.gov>
2019-09-18 16:05:10 -0700

    Trying to build in predefined directories

--- scripts/gitlab/build.sh
+++ scripts/gitlab/build.sh
@@ -10,15 +10,16 @@ set -e
 
 hostname
 
-BUILD_DIRECTORY="build_${SYS_TYPE}_${COMPILER}"
+PROJECT_DIRECTORY="$(pwd)"
+BUILD_DIRECTORY="${BUILD_ROOT}/build_${SYS_TYPE}_${COMPILER}"
 CCONF="host-configs/${SYS_TYPE}/${COMPILER}.cmake" 
 
 if [[ "${1}" != "--test-only" ]]
 then
     # If building, then delete everything first
 
-    rm -rf ${BUILD_DIRECTORY} 2>/dev/null
-    mkdir ${BUILD_DIRECTORY}
+    rm -rf ${BUILD_DIRECTORY}
+    mkdir -p ${BUILD_DIRECTORY}
 fi
 
 # Assert that build directory exist (mainly for --test-only mode)
@@ -28,22 +29,23 @@ then
     exit 1
 fi
 
-# Always go to build directory 
+# Always go to build directory
 cd ${BUILD_DIRECTORY}
 
 # Build
 if [[ "${1}" != "--test-only" ]]
 then
     cmake \
-      -C ../.gitlab/conf/${CCONF} \
-      -C ../${CCONF} \
-      ..
+      -C ${PROJECT_DIRECTORY}/.gitlab/conf/${CCONF} \
+      -C ${PROJECT_DIRECTORY}/${CCONF} \
+      ${PROJECT_DIRECTORY}
     cmake --build . -j 4
 fi
 
 # Test
 if [[ "${1}" != "--build-only" ]] 
 then
-    # If not building, then don’t
-    make test
+    ctest -T test
+    tree Testing
+    cp Testing/*/Test.xml ${PROJECT_DIRECTORY}
 fi
