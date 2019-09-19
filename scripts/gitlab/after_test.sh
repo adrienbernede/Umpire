849db177bcd816db11011f8b66859a5e88ec6431
Adrien M. Bernede <bernede1@llnl.gov>
2019-09-19 13:10:11 -0700

    fix

--- scripts/gitlab/after_test.sh
+++ scripts/gitlab/after_test.sh
@@ -13,6 +13,9 @@ BUILD_DIRECTORY="${BUILD_ROOT}/build_${SYS_TYPE}_${COMPILER}"
 
 . ${PYTHON_ENVIRONMENT_PATH}/bin/activate
 
+echo "moving to ${BUILD_DIRECTORY}"
+cd ${BUILD_DIRECTORY}
+
 python ${PROJECT_DIRECTORY}/scripts/gitlab/junit/ctest2junit.py ./ ${PROJECT_DIRECTORY}/scripts/gitlab/junit/ctest2junit.xsl > test.xml
 
 cp test.xml ${PROJECT_DIRECTORY}/
