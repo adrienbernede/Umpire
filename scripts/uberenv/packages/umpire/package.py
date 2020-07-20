e9a42ef77feaeb9ea9b169bc08742300f0b2c171
Adrien M. BERNEDE <bernede1@llnl.gov>
2020-07-20 14:22:16 -0700

    Fixing umpire spack package.py

--- scripts/uberenv/packages/umpire/package.py
+++ scripts/uberenv/packages/umpire/package.py
@@ -111,8 +111,8 @@ class Umpire(CMakePackage, CudaPackage):
         host_config_path = "%s-%s-%s%s-%s.cmake" % (socket.gethostname().rstrip('1234567890'),
                                                self._get_sys_type(spec),
                                                spec.compiler,
-                                               spec.hash,
-                                               var)
+                                               var,
+                                               spec.dag_hash())
         dest_dir = self.stage.source_path
         host_config_path = os.path.abspath(pjoin(dest_dir, host_config_path))
         return host_config_path
@@ -252,11 +252,19 @@ class Umpire(CMakePackage, CudaPackage):
         else:
             cfg.write(cmake_cache_option("ENABLE_CUDA", False))
 
-        cfg.write(cmake_cache_option("ENABLE_C={0}".format('+c' in spec)))
-        cfg.write(cmake_cache_option("ENABLE_FORTRAN={0}".format('+fortran' in spec)))
-        cfg.write(cmake_cache_option("ENABLE_NUMA={0}".format('+numa' in spec)))
-        cfg.write(cmake_cache_option("ENABLE_OPENMP={0}".format('+openmp' in spec)))
-        cfg.write(cmake_cache_option("ENABLE_TESTS={0}".format(self.run_tests)))
+        cfg.write(cmake_cache_option("ENABLE_C", '+c' in spec))
+        cfg.write(cmake_cache_option("ENABLE_FORTRAN", '+fortran' in spec))
+        cfg.write(cmake_cache_option("ENABLE_NUMA", '+numa' in spec))
+        cfg.write(cmake_cache_option("ENABLE_OPENMP", '+openmp' in spec))
+        cfg.write(cmake_cache_option("ENABLE_TESTS", self.run_tests))
+
+        #######################
+        # Close and save
+        #######################
+        cfg.write("\n")
+        cfg.close()
+
+        print("OUT: host-config file {0}".format(host_config_path))
 
     def cmake_args(self):
         spec = self.spec
