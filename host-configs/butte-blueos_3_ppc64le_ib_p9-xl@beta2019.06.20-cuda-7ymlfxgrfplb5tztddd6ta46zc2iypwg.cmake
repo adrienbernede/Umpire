cc81711dd41972808644987f1fa528b975a22dfb
Adrien M. BERNEDE <bernede1@llnl.gov>
2020-07-22 18:07:22 -0700

    Fix missing cuda in xl job

--- host-configs/butte-blueos_3_ppc64le_ib_p9-xl@beta2019.06.20-cuda-7ymlfxgrfplb5tztddd6ta46zc2iypwg.cmake
+++ host-configs/butte-blueos_3_ppc64le_ib_p9-xl@beta2019.06.20-cuda-7ymlfxgrfplb5tztddd6ta46zc2iypwg.cmake
@@ -22,7 +22,15 @@ set(CMAKE_C_COMPILER "/usr/tce/packages/xl/xl-beta-2019.06.20/bin/xlc" CACHE PAT
 
 set(CMAKE_CXX_COMPILER "/usr/tce/packages/xl/xl-beta-2019.06.20/bin/xlc++" CACHE PATH "")
 
-set(ENABLE_CUDA OFF CACHE BOOL "")
+#------------------------------------------------------------------------------
+# Cuda
+#------------------------------------------------------------------------------
+
+set(ENABLE_CUDA ON CACHE BOOL "")
+
+set(CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-10.1.168" CACHE PATH "")
+
+set(CMAKE_CUDA_COMPILER "${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc" CACHE PATH "")
 
 set(ENABLE_C ON CACHE BOOL "")
 
